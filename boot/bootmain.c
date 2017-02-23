6b491de18be7bd9656481b7cc042aa0be4abbff7
Ji Zhou <mail@zhouji.me>
2017-02-24 01:42:57 +0800

    [lab1]Complete Bootloader

--- boot/bootmain.c
+++ boot/bootmain.c
@@ -23,10 +23,57 @@
 #include <sys/types.h>
 #include <aim/boot.h>
 #include <elf.h>
+#include <asm.h>
+
+struct elfhdr * ELFHDR = ((struct elfhdr *)0x10000);
+
+static void waitdisk(void) {
+    while ((inb(0x1F7) & 0xC0) != 0x40);
+}
+
+static void readsect(void *dst, uint32_t secno) {
+    waitdisk();
+
+    outb(0x1F2, 1);
+    outb(0x1F3, secno & 0xFF);
+    outb(0x1F4, (secno >> 8) & 0xFF);
+    outb(0x1F5, (secno >> 16) & 0xFF);
+    outb(0x1F6, ((secno >> 24) & 0xF) | 0xE0);
+    outb(0x1F7, 0x20);
+
+    waitdisk();
+
+    insl(0x1F0, dst, SECT_SIZE);
+}
+
+static void readseg(uintptr_t va, uint32_t count, uint32_t offset) {
+    uintptr_t end_va = va + count;
+
+    va -= offset % SECT_SIZE;
+
+    uint32_t secno = (offset / SECT_SIZE) + 1;
+
+    for (; va < end_va; va += SECT_SIZE, secno ++) {
+        readsect((void *)va, secno);
+    }
+}
 
 __noreturn
 void bootmain(void)
 {
+	readseg((uintptr_t)ELFHDR, SECT_SIZE * 8, 0);
+
+    struct proghdr *ph, *eph;
+
+    ph = (struct proghdr *)((uintptr_t)ELFHDR + ELFHDR->e_phoff);
+    eph = ph + ELFHDR->e_phnum;
+    for (; ph < eph; ph ++) {
+        readseg(ph->p_va & 0xFFFFFF, ph->p_memsz, ph->p_offset);
+    }
+
+    ((void (*)(void))(ELFHDR->e_entry)();
+
+bad:
 	while (1);
 }
 
